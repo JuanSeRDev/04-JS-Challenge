ec8c2c391a0f41d2a0e7809d8660059435d55762
JuanSeCode <juansebastianrodriguezr@gmail.com>
2024-03-07 15:27:11 -0500

    correccion de errores en HTML y CSS, comienzo con JS

--- Script.js
+++ Script.js
@@ -1,3 +1,12 @@
-form = document.getElementById("suscribe-form").addEventListener("submit",(e)=>{
-    
+form = document.getElementById("suscribe-form").addEventListener("submit", ()=>{
+    let emailImput = document.querySelector(".imput");
+    let errorMessage = document.getElementById("error-message");
+
+    if (isvalidEmail(emailImput.value)) {
+        errorMessage.classList.remove(".hidden");
+    }else {
+        errorMessage.classList.add(".hiden")
+    }
 })
+
+
